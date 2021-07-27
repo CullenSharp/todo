5f231408ff001a08db46c1ebda8d743264415235
Cullen Sharp <cullen.sharp100@gmail.com>
2021-07-27 16:40:00 -0700

    Move form to own component

---
+++ src/components/todo/form.js
@@ -0,0 +1,38 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import propTypes from 'prop-types';
+
+function Form({ handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit}>
+
+      <h2>Add To Do Item</h2>
+
+      <label>
+        <span>To Do Item</span>
+        <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
+      </label>
+
+      <label>
+        <span>Assigned To</span>
+        <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
+      </label>
+
+      <label>
+        <span>Difficulty</span>
+        <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
+      </label>
+
+      <label>
+        <button type="submit">Add Item</button>
+      </label>
+    </form>
+  );
+}
+Form.propTypes = {
+  handleChange: propTypes.func.isRequired,
+  handleSubmit: propTypes.func.isRequired,
+};
+
+export default Form;
