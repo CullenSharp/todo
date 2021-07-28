9a885b81e5824ad424bcbbfc5319cda8d2c20d06
Cullen Sharp <cullen.sharp100@gmail.com>
2021-07-28 15:57:03 -0700

    Add logic for pagination

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -4,20 +4,26 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable no-param-reassign */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { v4 as uuid } from 'uuid';
-import { Navbar } from '@blueprintjs/core';
+import { Navbar, Button } from '@blueprintjs/core';
 
+import { ButtonGroup } from 'react-bootstrap';
 import Form from './form';
 import List from './list';
+import { SettingsContext } from '../../context/settings';
 
 import useForm from '../../hooks/form';
 
 const ToDo = () => {
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
+  const [pages, setPages] = useState([]);
+  const [activePage, setActivePage] = useState([]);
   // eslint-disable-next-line no-use-before-define
   const { handleChange, handleSubmit } = useForm(addItem);
+
+  const { pageNumber } = useContext(SettingsContext);
 
   function addItem(item) {
     item.id = uuid();
@@ -48,18 +54,54 @@ const ToDo = () => {
     document.title = `To Do List: ${incomplete}`;
   }, [list]);
 
+  useEffect(() => {
+    let beginning = 0;
+    let end = pageNumber;
+    const segments = [];
+    if (list.length >= pageNumber) {
+      while (list.length > beginning) {
+        const page = list.slice(beginning, end);
+        segments.push(page);
+
+        beginning += pageNumber;
+        end += pageNumber;
+      }
+      setPages([...segments]);
+      setActivePage([...segments[0]]);
+    }
+  }, [list]);
+
   return (
     <>
       <Navbar>
-        <div className="bp3-navbar-heading">
-          To Do List:
-          {incomplete}
-          {' '}
-          items pending
-        </div>
+        <Navbar.Group>
+          <Navbar.Heading> To Do </Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="home" text="Home" />
+          <Button className="bp3-minimal" icon="document" text="Files" />
+        </Navbar.Group>
       </Navbar>
-      <Form handleChange={handleChange} handleSubmit={handleSubmit} />
-      <List list={list} toggleComplete={toggleComplete} />
+      <div id="content-wrapper">
+        <Form handleChange={handleChange} handleSubmit={handleSubmit} />
+
+        <List
+          list={list.length >= pageNumber ? activePage : list}
+          toggleComplete={toggleComplete}
+        />
+      </div>
+      { pages.length
+        && (
+          <ButtonGroup>
+            { pages.map((page, idx) => (
+              <Button
+                key={pages.indexOf(page)}
+                onClick={() => setActivePage(pages[idx])}
+              >
+                {pages.indexOf(page) + 1}
+              </Button>
+            ))}
+          </ButtonGroup>
+        )}
     </>
   );
 };
