f9f52b0f8384a4116e756f613ac33a29bfadc19b
Cullen Sharp <cullen.sharp100@gmail.com>
2021-07-28 20:02:37 -0700

    First version of auth component

---
+++ src/components/auth/auth.jsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import propTypes from 'prop-types';
+
+import { AuthorizationContext } from '../../context/authorization';
+
+function Auth({ capability, children }) {
+  const { isAuthorized } = useContext(AuthorizationContext);
+
+  if (isAuthorized(capability)) {
+    return (
+      { children }
+    );
+  }
+  return null;
+}
+Auth.propTypes = {
+  children: propTypes.element.isRequired,
+  capability: propTypes.string.isRequired,
+};
+
+export default Auth;
